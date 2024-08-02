f218a6f901c10041b3a06bbd7f20eeb13054f822
Art Iffy <iffyart1234@gmail.com>
2024-08-03 02:13:15 +0800

    chore: Add Bootstrap and custom styles for header and google icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import Header from './components/Header';
+import './styles/style.css';
 
 function App() {
   return (
     <div>
       <Header />
-      Hello React , I am App.js
+      <div
+        className='btn-group'
+        role='group'
+        aria-label='Basic mixed styles example'
+      >
+        <span className='material-symbols-outlined'>logout</span>
+        <button type='button' className='btn btn-danger'>
+          Left
+        </button>
+        <button type='button' className='btn btn-warning'>
+          Middle
+        </button>
+        <button type='button' className='btn btn-success'>
+          Right
+        </button>
+      </div>
     </div>
   );
 }
