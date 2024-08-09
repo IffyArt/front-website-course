a7a66cbc0fb1e4c0a70a5fd5ea57196b2cd81da1
Art Iffy <iffyart1234@gmail.com>
2024-08-10 01:40:38 +0800

    chore: Update react-router-dom to version 6.26.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Navbar from './components/Nav';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <Navbar />
+        Home
+      </div>
+    ),
+  },
+  {
+    path: '/news',
+    element: (
+      <div>
+        <Navbar />
+        News
+      </div>
+    ),
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
